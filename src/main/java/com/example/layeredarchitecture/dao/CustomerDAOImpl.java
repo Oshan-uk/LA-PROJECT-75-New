45f876c0bf94b8d4787e281ef7d3adbee41d3a1d
Oshan <git config --global user.email>
2026-01-31 21:48:33 +0530

    Update project

---
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -0,0 +1,77 @@
+package com.example.layeredarchitecture.dao;
+
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.CustomerDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl implements CustomerDAO {
+    @Override
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
+        ResultSet rst=CRUDUtil.execute("SELECT * FROM Customer");
+        ArrayList<CustomerDTO> customers=new ArrayList<CustomerDTO>();
+        while (rst.next()) {
+            String id=rst.getString("id");
+            String name=rst.getString("name");
+            String address=rst.getString("address");
+            CustomerDTO customerDTO=new CustomerDTO(id,name,address);
+            customers.add(customerDTO);
+        }
+        return customers;
+    }
+    @Override
+    public void saveCustomer(String id, String name, String address) throws SQLException, ClassNotFoundException {
+        CRUDUtil.execute("INSERT INTO Customer (id,name, address) VALUES (?,?,?)",id,name,address);
+    }
+    @Override
+    public void updateCustomer(String id, String name, String address) throws SQLException, ClassNotFoundException {
+//        Connection connection = DBConnection.getDbConnection().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+//        pstm.setString(1, name);
+//        pstm.setString(2, address);
+//        pstm.setString(3, id);
+//        pstm.executeUpdate();
+        CRUDUtil.execute("UPDATE Customer SET name=?, address=? WHERE id=?",id,name,address);
+    }
+    @Override
+    public void deleteCustomer(String id) throws SQLException, ClassNotFoundException {
+//        Connection connection = DBConnection.getDbConnection().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
+//        pstm.setString(1, id);
+//        pstm.executeUpdate();
+        CRUDUtil.execute("DELETE FROM Customer WHERE id=?",id);
+
+    }
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        ResultSet rst =CRUDUtil.execute("SELECT id FROM Customer ORDER BY id DESC LIMIT 1;");
+        if (rst.next()) {
+            String id = rst.getString("id");
+            int newCustomerId = Integer.parseInt(id.replace("C00-", "")) + 1;
+            return String.format("C00-%03d", newCustomerId);
+        } else {
+            return "C00-001";
+        }
+    }
+    @Override
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
+        ResultSet rst=CRUDUtil
+                .execute("SELECT * FROM Customer WHERE id=?", id);
+        return rst.next();
+    }
+    @Override
+    public CustomerDTO findCustomer(String id) throws SQLException, ClassNotFoundException {
+        ResultSet rst = CRUDUtil.execute("SELECT * FROM Customer WHERE id=?",id);
+        if (rst.next()) {
+            return new CustomerDTO(
+                    rst.getString("id"),
+                    rst.getString("name"),
+                    rst.getString("address")
+            );
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/example/layeredarchitecture/dao/ItemDAOImpl.java
@@ -0,0 +1,98 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.CustomerDTO;
+import com.example.layeredarchitecture.model.ItemDTO;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ItemDAOImpl {
+    public ArrayList<ItemDTO> loadAllItems() throws SQLException, ClassNotFoundException {
+//        Connection connection = DBConnection.getDbConnection().getConnection();
+//        Statement stm = connection.createStatement();
+//        ResultSet rst = stm.executeQuery("SELECT * FROM Item");
+        ResultSet rst = CRUDUtil.execute("SELECT * FROM Item");
+        ArrayList<ItemDTO> items = new ArrayList<>();
+        while (rst.next()) {
+            String code = rst.getString("code");
+            String description = rst.getString("description");
+            BigDecimal unitPrice = rst.getBigDecimal("unitPrice");
+            Integer qtyOnHand = rst.getInt("qtyOnHand");
+            ItemDTO ItemDTO = new ItemDTO(code, description, unitPrice, qtyOnHand);
+            items.add(ItemDTO);
+        }
+        return items;
+
+    }
+
+    public void saveItem(String code, String description, BigDecimal unitPrice, int qtyOnHand) throws SQLException, ClassNotFoundException {
+//            Connection connection = DBConnection.getDbConnection().getConnection();
+//            PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+//            pstm.setString(1, itemDTO.getCode());
+//            pstm.setString(2, itemDTO.getDescription());
+//            pstm.setBigDecimal(3, itemDTO.getUnitPrice());
+//            pstm.setInt(4, itemDTO.getQtyOnHand());
+//            pstm.executeUpdate();
+        CRUDUtil.execute("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)", code, description, unitPrice, qtyOnHand);
+    }
+
+    public boolean updateItem(String code, String description, BigDecimal unitPrice, int qtyOnHand) throws SQLException, ClassNotFoundException {
+
+        return CRUDUtil.execute("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?", description, unitPrice, qtyOnHand, code);
+    }
+
+    public boolean deleteItem(String code)
+            throws SQLException, ClassNotFoundException {
+
+        return CRUDUtil.execute(
+                "DELETE FROM Item WHERE code=?",
+                code.trim()
+        );
+    }
+
+
+    public boolean existsItem(String itemCode) throws SQLException, ClassNotFoundException {
+//            Connection connection = DBConnection.getDbConnection().getConnection();
+//
+//            PreparedStatement pstm = connection.prepareStatement("SELECT code FROM Item WHERE code=?");
+//            pstm.setString(1, itemCode);
+//            return pstm.executeQuery().next();
+        return CRUDUtil.execute("SELECT code FROM Item WHERE code=?", itemCode);
+    }
+
+    public String generateNewId() throws SQLException, ClassNotFoundException {
+//        Connection connection = DBConnection.getDbConnection().getConnection();
+//        ResultSet rst = connection.createStatement().executeQuery("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
+
+        ResultSet rst = CRUDUtil.execute("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
+        if (rst.next()) {
+            String id = rst.getString("code");
+            int newItemId = Integer.parseInt(id.replace("I00-", "")) + 1;
+            return String.format("I00-%03d", newItemId);
+        } else {
+            return "I00-001";
+        }
+    }
+
+    public ItemDTO findItem(String itemCode) throws SQLException, ClassNotFoundException {
+//        Connection connection = DBConnection.getDbConnection().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item WHERE code=?");
+//        pstm.setString(1, itemCode + "");
+//        ResultSet rst = pstm.executeQuery();
+        ResultSet rst = CRUDUtil.execute("SELECT * FROM Item WHERE code=?", itemCode);
+
+        if (rst.next()) {
+            return new ItemDTO(
+                    rst.getString("code"),
+                    rst.getString("description"),
+                    rst.getBigDecimal("unitPrice"),
+                    rst.getInt("qtyOnHand")
+            );
+        }
+        return null;
+    }
+}
+
+
